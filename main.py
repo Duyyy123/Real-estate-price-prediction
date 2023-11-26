0230dc09aeb9b3a744706e2733a8518610e267f8
Duy <trinhchanduybojack@gmail.com>
2023-11-26 11:32:39 +0700

    turn label type from <class 'pandas.core.series.Series'> to <class 'pandas.core.frame.DataFrame'>

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ df = pd.read_csv("real_estate.csv")
 
 # feature extraction and labeling
 data = df.drop(["No", "Y house price of unit area"], axis = 1)
-label = df["Y house price of unit area"]
+label = df[["Y house price of unit area"]]
 
 # data splitting
 train_data, train_label, test_data, test_label = train_test_split(data, label, test_size = 0.1, random_state = 50)
@@ -20,7 +20,14 @@ test_label = test_label.to_numpy()
 train_label = train_label.to_numpy()
 
 # show data
+print("features: \n", type(data))
+print("label: \n", type(label))
 print("train data shape: ", train_data.shape)
 print("train label shape: ", train_label.shape)
 print("test data shape: ", test_data.shape)
 print("test label shape: ", test_label.shape)
+
+
+ <class 'pandas.core.series.Series'>
+
+<class 'pandas.core.frame.DataFrame'>
