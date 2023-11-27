e00adca0f46ec41252d49e470cd700195ec784f1
Duy <trinhchanduybojack@gmail.com>
2023-11-27 19:33:35 +0700

    add demo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ data = df.drop(["No", "Y house price of unit area"], axis = 1)
 label = df[["Y house price of unit area"]]
 
 # data splitting
-train_data, train_label, test_data, test_label = train_test_split(data, label, test_size = 0.1, random_state = 50)
+train_data, test_data, train_label, test_label = train_test_split(data, label, test_size = 0.1, random_state = 50)
 
 # data normalization
 test_data = test_data.to_numpy()
@@ -20,14 +20,22 @@ test_label = test_label.to_numpy()
 train_label = train_label.to_numpy()
 
 # show data
-print("features: \n", type(data))
-print("label: \n", type(label))
+
+# show feature and label
+print("data: \n", data)
+print("label: \n", label)
+print()
+# show train set
+print("train data head: \n", train_data[:5, :])
+print("train label head: \n", train_label[:5, :])
+
 print("train data shape: ", train_data.shape)
 print("train label shape: ", train_label.shape)
+print()
+
+# show test set
+print("test data head : \n", test_data[:5, :])
+print("test label head: \n", test_label[:5, :])
+
 print("test data shape: ", test_data.shape)
 print("test label shape: ", test_label.shape)
-
-
- <class 'pandas.core.series.Series'>
-
-<class 'pandas.core.frame.DataFrame'>
