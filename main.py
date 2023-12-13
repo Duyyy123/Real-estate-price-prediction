ac7b3ed87c1dafc9a3ea88407b5319e6bc960be1
duyyy12 <trinhchanduybojack@gmail.com>
2023-12-13 10:59:10 +0700

    MSE test

--- main.py
+++ main.py
@@ -17,24 +17,58 @@ label = df[:, 7].reshape(-1)
 # data splitting
 train_data, test_data, train_label, test_label = model_selection.train_test_split(data, label, test_size = 0.1, random_state = 50)
 
-# show data
+#Set values
+alpha = 0.002302
+epsilon = 1e-6
+max_loops = 1000
 
-# show feature and label
-print("data: \n", data)
-print("label: \n", label)
-print()
+#random w, b
+w = np.random.rand(1, 6)
+b = np.random.rand(1, 1)
 
-# show train set
-print("train data head: \n", train_data[:5, :])
-print("train label head: \n", train_label[:5])
+#compute L
+def compute(w, b):
+    N = len(train_data)
+    L = 0.0
+    for i in range (N):
+        L += 0.5 * ((w@train_data[i] + b - train_label[i]) ** 2)
+    return L
 
-print("train data shape: ", train_data.shape)
-print("train label shape: ", train_label.shape)
-print()
+#derivative
+def derivative(w, b):
+    N = len(train_data)
+    dw = np.zeros_like(w)
+    db = 0.0
+    for i in range (N):
+        dw += (w @ train_data[i] + b - train_label[i]) * train_data[i]
+        db += w @ train_data[i] + b - train_label[i]
+    return dw, db
 
-# show test set
-print("test data head : \n", test_data[:5, :])
-print("test label head: \n", test_label[:5])
+#gradient_descent
+k = 0
+pre_L = float('inf')
+while k < max_loops:
+    dw, db = derivative(w,b)
+    w = w - alpha * dw
+    b = b - alpha * db
+    
+    L = compute(w,b)
+    
+    if abs(L - pre_L) < epsilon:
+        break
+    
+    pre_L = L
+    k += 1
 
-print("test data shape: ", test_data.shape)
-print("test label shape: ", test_label.shape)
+#y^
+M = len(test_data)
+predict = np.zeros_like(test_label)
+for i in range(M):
+    predict[i] = w @ test_data[i] + b
+
+#MSE
+mse = 0.0
+for i in range(M):
+    mse += (test_label[i] - predict[i]) ** 2
+mse /= M
+print(mse) 
